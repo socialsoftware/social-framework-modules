e3ab36f7504ea9533ea90f37d4b32f5d189b32c9
Sergio Silva <sergio.miguel.inacio@gmail.com>
2013-07-16 01:06:10 +0100

    Versão final

--- data-model/src/main/java/pt/ist/data/DataObject.java
+++ data-model/src/main/java/pt/ist/data/DataObject.java
@@ -1,6 +1,7 @@
 package pt.ist.data;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import pt.ist.authentication.User;
 import pt.ist.util.time.EffectivePeriod;
@@ -44,12 +45,12 @@ public class DataObject extends DataObject_Base {
     public void createNewVersionByMergingWith(User author, DataObjectVersion dataObjectVersion) {
 //        DataObjectVersion newVersion = new DataObjectVersion();
 //        newVersion.init(dataObjectVersion.getType(), dataObjectVersion.getExternalizedValue(), author);
-    	DataObjectVersion newVersion = dataObjectVersion.newVersionOf(author);
+    	DataObjectVersion newVersion = dataObjectVersion.getNewVersion(author);
         newVersion.setSourceMergeVersion(dataObjectVersion);
         addVersion(newVersion);
     }
 
-    public void setExternalizedValue(String externalizedValue, User author) {
+    public void setExternalizedValue(String externalizedValue, User author, boolean executeExplicit) {
         DataObjectVersion lastVersion = getLastVersion();
         DataObjectVersion newVersion = new DataObjectVersion();
         newVersion.init(lastVersion.getType(), externalizedValue, author);
@@ -57,41 +58,77 @@ public class DataObject extends DataObject_Base {
         DataObjectVersion currentVersion = newVersion;
         while(currentVersion != null) {
             if(currentVersion.getDataObject().hasAnySourceSynchronizationConfiguration()) {
-                handleSourceSynchronizationConfigurations(currentVersion.getDataObject().getSourceSynchronizationConfigurationSet());
+                handleSourceSynchronizationConfigurations(currentVersion.getDataObject().getSourceSynchronizationConfigurationSet(), executeExplicit, null);
             }
             if(currentVersion.getDataObject().hasAnyTargetSynchronizationConfiguration()) {
-                handleTargetSynchronizationConfigurations(currentVersion.getDataObject().getTargetSynchronizationConfigurationSet());
+                handleTargetSynchronizationConfigurations(currentVersion.getDataObject().getTargetSynchronizationConfigurationSet(), executeExplicit,  null);
             }
             currentVersion = currentVersion.getPreviousVersion();
         }
     }
  
-    public void setNewDataObjectVersion(DataObjectVersion newVersion, User author) {
+    public void setNewDataObjectVersion(DataObjectVersion newVersion, User author, boolean executeExplicit) {
         addVersion(newVersion);
         DataObjectVersion currentVersion = newVersion;
         while( currentVersion != null) {
             if(currentVersion.getDataObject().hasAnySourceSynchronizationConfiguration()) {
-                handleSourceSynchronizationConfigurations(currentVersion.getDataObject().getSourceSynchronizationConfigurationSet());
+                handleSourceSynchronizationConfigurations(currentVersion.getDataObject().getSourceSynchronizationConfigurationSet(), executeExplicit, null);
             }
             if(currentVersion.getDataObject().hasAnyTargetSynchronizationConfiguration()) {
-                handleTargetSynchronizationConfigurations(currentVersion.getDataObject().getTargetSynchronizationConfigurationSet());
+                handleTargetSynchronizationConfigurations(currentVersion.getDataObject().getTargetSynchronizationConfigurationSet(), executeExplicit, null);
             }
             currentVersion = currentVersion.getPreviousVersion();
         }
     }
     
-    private void handleSourceSynchronizationConfigurations(Set<SynchronizationConfiguration> sourceConfigurationSet) {
+    public void setNewDataObjectVersionOfLastVersion(DataObjectVersion newVersion, User author, boolean executeExplicit, List<Long> listDataObjectTargets) {
+        addVersion(newVersion);
+        DataObjectVersion currentVersion = newVersion;
+        if(currentVersion.getDataObject().hasAnySourceSynchronizationConfiguration()) {
+            handleSourceSynchronizationConfigurations(currentVersion.getDataObject().getSourceSynchronizationConfigurationSet(), executeExplicit, listDataObjectTargets);
+        }
+        if(currentVersion.getDataObject().hasAnyTargetSynchronizationConfiguration()) {
+            handleTargetSynchronizationConfigurations(currentVersion.getDataObject().getTargetSynchronizationConfigurationSet(), executeExplicit, listDataObjectTargets);
+        }
+        //currentVersion = currentVersion.getPreviousVersion();
+        
+    }
+
+    private void handleSourceSynchronizationConfigurations(Set<SynchronizationConfiguration> sourceConfigurationSet, boolean executeExplicit, List<Long> listDataObjectTargets) {
         for(SynchronizationConfiguration sourceConfig : sourceConfigurationSet) {
             if(sourceConfig.getSourcePolicy().equals(SynchronizationPolicy.AUTO)) {
                 sourceConfig.getTargetDataObject().createNewVersionByMergingWith(sourceConfig.getAuthor(), getLastVersion());
             }
+            else if(sourceConfig.getSourcePolicy().equals(SynchronizationPolicy.EXPLICIT) && executeExplicit == true ) {
+            	if(listDataObjectTargets != null)
+            	{
+            		for(long dataObjectOID : listDataObjectTargets)
+            		{
+            			if(dataObjectOID == sourceConfig.getTargetDataObject().getOID()){
+            				sourceConfig.getTargetDataObject().createNewVersionByMergingWith(sourceConfig.getAuthor(), getLastVersion());
+            			}
+            		}
+            	}
+            }
         }
     }
 
-    private void handleTargetSynchronizationConfigurations(Set<SynchronizationConfiguration> targetConfigurationSet) {
+    private void handleTargetSynchronizationConfigurations(Set<SynchronizationConfiguration> targetConfigurationSet, boolean executeExplicit, List<Long> listDataObjectTargets) {
         for(SynchronizationConfiguration targetConfig : targetConfigurationSet) {
             if(targetConfig.getTargetPolicy().equals(SynchronizationPolicy.AUTO)) {
                 targetConfig.getSourceDataObject().createNewVersionByMergingWith(targetConfig.getAuthor(), getLastVersion());
+            }
+            else if(targetConfig.getTargetPolicy().equals(SynchronizationPolicy.EXPLICIT) && executeExplicit == true ) {
+            	if(listDataObjectTargets != null)
+            	{
+            		for(long dataObjectOID : listDataObjectTargets)
+            		{
+            			if(dataObjectOID == targetConfig.getSourceDataObject().getOID()){
+            				getLastVersion().setReadyToUpdate(true);
+            				 //targetConfig.getSourceDataObject().createNewVersionByMergingWith(targetConfig.getAuthor(), getLastVersion());
+            			}
+            		}
+            	}
             }
         }
     }
@@ -125,5 +162,50 @@ public class DataObject extends DataObject_Base {
             currentVersion = currentVersion.getPreviousVersion();
         }
         return mergeRequestSet;
+    }
+    
+    public Set<MergeRequest> getMergeRequestSetBasedOnMergedConnections(){
+    	Set<MergeRequest> mergeRequestSet = new HashSet<MergeRequest>();
+    	
+    	mergeRequestSet.addAll(getMergeRequestSetBasedOnMergedConnectionsFromTargetDataObject());
+    	mergeRequestSet.addAll(getMergeRequestSetBasedOnMergedConnectionsFromSourceDataObject());
+    	
+    	return mergeRequestSet;
+    }
+    
+    public Set<MergeRequest> getMergeRequestSetBasedOnMergedConnectionsFromTargetDataObject(){
+    	Set<MergeRequest> mergeRequestSet = new HashSet<MergeRequest>();
+        DataObjectVersion currentVersion = getLastVersion();
+        if(currentVersion.getDataObject().hasAnyTargetSynchronizationConfiguration() == true) {
+            for(SynchronizationConfiguration targetConf : currentVersion.getDataObject().getTargetSynchronizationConfiguration()) {
+            	if(targetConf.getTargetPolicy().equals(SynchronizationPolicy.EXPLICIT)){
+	            	if(targetConf.getSourceDataObject().getLastVersion().hasSourceMergeVersion() == false){
+	            		if(targetConf.getSourceDataObject().getLastVersion().getReadyToUpdate() == true && targetConf.getSourceDataObject().getLastVersion().getIgnoreInMergeRequest() == false)
+	            		{
+	            			mergeRequestSet.add(new MergeRequest(targetConf.getSourceDataObject().getLastVersion(), currentVersion.getDataObject()));
+	            		}
+	            	}
+            	}
+            }
+        }
+        return mergeRequestSet;
+    }
+    
+    public Set<MergeRequest> getMergeRequestSetBasedOnMergedConnectionsFromSourceDataObject(){
+    	Set<MergeRequest> mergeRequestSet = new HashSet<MergeRequest>();
+        DataObjectVersion currentVersion = getLastVersion();
+        if(currentVersion.getDataObject().hasAnySourceSynchronizationConfiguration() == true) {
+            for(SynchronizationConfiguration sourceConf : currentVersion.getDataObject().getSourceSynchronizationConfiguration()) {
+            	if(sourceConf.getSourcePolicy().equals(SynchronizationPolicy.EXPLICIT)){
+	            	if(sourceConf.getTargetDataObject().getLastVersion().hasAnyTargetMergedVersion() == false){
+	            		if(sourceConf.getTargetDataObject().getLastVersion().getReadyToUpdate() == true && sourceConf.getTargetDataObject().getLastVersion().getIgnoreInMergeRequest() == false)
+	            		{
+	            			mergeRequestSet.add(new MergeRequest(sourceConf.getTargetDataObject().getLastVersion(), currentVersion.getDataObject()));
+	            		}
+	            	}
+             	}
+            }
+        }
+        return mergeRequestSet; 
     }
 }
--- data-model/src/main/java/pt/ist/data/DataObjectVersion.java
+++ data-model/src/main/java/pt/ist/data/DataObjectVersion.java
@@ -1,11 +1,18 @@
 package pt.ist.data;
 
 import java.util.HashMap;
+import java.util.Map;
+
 import pt.ist.authentication.User;
 import pt.ist.util.time.EffectivePeriod;
 
 public class DataObjectVersion extends DataObjectVersion_Base {
 
+    public void init(User author, HashMap<String, Object> attributes) {
+        init(author);
+        setAttributes(attributes);
+    }	
+	
     public void init(DataObjectType type, String externalizedValue, User author) {
         setType(type);
         setExternalizedValue(externalizedValue);
@@ -24,28 +31,57 @@ public class DataObjectVersion extends DataObjectVersion_Base {
     		forkedDataObjectVersion =  new DataObjectVersion();
     	}
     	forkedDataObjectVersion.init(author);
-    	for(DataObjectAttribute attribute : getAttribute()){
-    		DataObjectAttribute newAttribute = new DataObjectAttribute();
-    		newAttribute.init(attribute.getLabel(), attribute.getType(), attribute.getExternalizedValue());
-    		forkedDataObjectVersion.addAttribute(newAttribute);
-    	}
+    	forkedDataObjectVersion.setAttributes(getAttributes());
     	return forkedDataObjectVersion;
     }
     
-    public DataObjectVersion newVersionOf(User author){
-    	DataObjectVersion newVersion = new DataObjectVersion();
-        newVersion.init(this.getType(), this.getExternalizedValue(), author);
+    public DataObjectVersion getNewVersion(User author, HashMap<String, Object> attributes){
+    	DataObjectVersion newVersion = new DataObjectVersion();	
+        newVersion.init(author, attributes);
         return newVersion;
     }
     
-    public HashMap<String, Object> getAllValues(){
+    public DataObjectVersion getNewVersion(User author){
+    	DataObjectVersion newVersion = new DataObjectVersion();
+        newVersion.init(author, this.getAttributes());
+        return newVersion;
+    }
+    
+    public static HashMap<String, Object> getDefaultAttributes()
+    {
     	HashMap<String, Object> values = new HashMap<String, Object>();
-   		values.put("Externalized Value",getExternalizedValue());
-   		values.put("OID", getDataObject().getOID());
-    	
+    	values.put("Name", null);
+    	values.put("Description", null);
+    	return values;
+    }
+    
+    public HashMap<String, Object> getAttributes(){
+    	HashMap<String, Object> values = new HashMap<String, Object>();
+
+    	if(getExternalizedValue() != null){
+    		values.put("Externalized Value", getExternalizedValue());
+    	}
+
     	for(DataObjectAttribute attribute : getAttribute()){
     		values.put(attribute.getLabel(), attribute.getExternalizedValue());
     	}
 		return values;
+    }
+    
+    public void setAttributes(HashMap<String, Object> attributes){
+    	if(attributes.size() != 0){
+    		for(Map.Entry<String , Object> attribute : attributes.entrySet()){
+    			if(attribute.getKey().equals("Externalized Value"))
+	    		{
+	    			setExternalizedValue((String)attribute.getValue());
+	    		}
+    			else
+    			{
+	    			DataObjectAttribute newAttribute = new DataObjectAttribute();
+		    		newAttribute.init(attribute.getKey(), DataObjectType.STRING, (String)attribute.getValue());
+		    		this.addAttribute(newAttribute);
+    			}
+        	}
+    	}
     }
 }
